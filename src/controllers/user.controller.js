52ce5f98c15a0665a74606a7f840ea2355c06db7
rishabhrajyadav <rishabhrajyadav10@gmail.com>
2024-04-02 10:43:42 +0530

    add user account management endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -266,36 +266,6 @@ const updateAccountDetails = asyncHandler(async(req,res) => {
          )
 })
 
-const updateUserCoverImage = asyncHandler(async(req,res) => {
-    const coverImageLocalPath = req.file?.path
-
-    if(!coverImageLocalPath){
-        throw new ApiError(400 , "Cover Image is missing")
-    }
-
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
-    if(!coverImage.url){
-        throw new ApiError(400 , "Error while uploading on cover image")
-    }
-
-    const user = await User.findByIdAndUpdate(
-        req.user?._id,
-        {
-            $set: {
-                coverImage: coverImage.url
-            }
-        },
-        {new :true} 
-        ).select("-password")
-
-        return res
-        .status(200)
-        .json(
-            new ApiResponse(200, user,"Cover image updated successfully")
-        )
-})
-
 const updateUserAvatar = asyncHandler(async(req,res) => {
     const avatarLocalPath = req.file?.path
 
@@ -326,6 +296,36 @@ const updateUserAvatar = asyncHandler(async(req,res) => {
         )   
 })
 
+const updateUserCoverImage = asyncHandler(async(req,res) => {
+    const coverImageLocalPath = req.file?.path
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400 , "Cover Image is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400 , "Error while uploading on cover image")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {new :true} 
+        ).select("-password")
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user,"Cover image updated successfully")
+        )
+})
+
 export {
     registerUser,
     loginUser,
@@ -334,5 +334,6 @@ export {
     changeCurrentPassword,
     getCurrentUser,
     updateAccountDetails,
-    updateUserAvatar
+    updateUserAvatar,
+    updateUserCoverImage
 }
